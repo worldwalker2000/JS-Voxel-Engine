905f81730be5cfdfc508209698aeb02a10b122ea
worldwalker2000 <space3334@gmail.com>
2018-04-08 14:51:24 -0700

    Finish Shader class
    Finished creating shader classes for webgl.

--- render/shader.js
+++ render/shader.js
@@ -7,5 +7,15 @@ function Shader(gl, src, type)
 	
 	this.create = function()
 	{
+		this.shader = this.gl.createShader(this.type);
+		this.gl.shaderSource(this.shader, this.src);
+		this.gl.compileShader(this.shader);
+		
+		if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS))
+		{
+			alert('An error occurred compiling the shader: ' + gl.getShaderInfoLog(shader));
+			gl.deleteShader(shader);
+			return null;
+		}
 	}
 }
---
+++ render/shaderProgram.js
@@ -0,0 +1,29 @@
+function ShaderProgram(gl, vertexShaderCode, fragmentShaderCode)
+{
+	this.gl = gl;
+	
+	this.vertexShaderCode = vertexShaderCode;
+	this.fregmentShaderCode = fragmentShaderCode;
+	
+	this.vertexShader = null;
+	this.fragmentShader = null;
+	
+	this.shaderProgram = null;
+	
+	this.create = function()
+	{
+		this.vertexShader = new Shader(this.gl, this.vertexShaderCode, this.gl.VERTEX_SHADER);
+		this.fragmentShader = new Shader(this.gl, this.fragmentShaderCode, this.gl.FRAGMENT_SHADER);
+		
+		this.shaderProgram = this.gl.createProgram();
+		this.gl.attachShader(this.shaderProgram, this.vertexShader.shader);
+		this.gl.attachShader(this.shaderProgram, this.fragmentShader.shader);
+		this.gl.linkProgram(this.shaderProgram);
+
+		if (!this.gl.getProgramParameter(shaderProgram, gl.LINK_STATUS))
+		{
+			alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
+			return null;
+		}
+	}
+}
