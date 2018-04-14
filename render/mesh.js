aa55b74e4d6f62f001057db6cbac3f7ce9ea0b78
worldwalker2000 <space3334@gmail.com>
2018-04-14 12:17:29 -0700

    Update Mesh class
    Added Vertex Array Object and "this." allot.

--- render/mesh.js
+++ render/mesh.js
@@ -9,16 +9,31 @@ function Mesh(gl, verts, texcords, indexs)
 	this.textureCordsBuffer = null;
 	this.indexBuffer = null;
 	
-	this.create = function()
+	this.vao = null;
+	
+	this.create = function(shaderProgram)
 	{
-		this.vertexBuffer = gl.createBuffer();
-		gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-		gl.bufferData(gl.ARRAY_BUFFER,
+		this.vao = this.gl.createVertexArray();
+		this.gl.bindVertexArray(vao);
+		
+		this.vertexBuffer = this.gl.createBuffer();
+		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, positionBuffer);
+		this.gl.bufferData(this.gl.ARRAY_BUFFER,
 				new Float32Array(this.verts),
-				gl.STATIC_DRAW);
+				this.gl.STATIC_DRAW);
+		this.gl.vertexAttribPointer(shaderProgram.attributeLocations.get("aVertexPosition"), 3, this.gl.FLOAT, false, 0, 0);
+		this.gl.enableVertexAttribArray(shaderProgram.attributeLocations.get("aVertexPosition"));
 
-		this.indexBuffer = gl.createBuffer();
-		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, colorBuffer);
-		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Float32Array(this.indexs), gl.STATIC_DRAW);
+		this.textureCordsBuffer = this.gl.createBuffer();
+		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, positionBuffer);
+		this.gl.bufferData(this.gl.ARRAY_BUFFER,
+				new Float32Array(this.texcords),
+				this.gl.STATIC_DRAW);
+		this.gl.vertexAttribPointer(shaderProgram.attributeLocations.get("aTextureCorrds"), 2, this.gl.FLOAT, false, 0, 0);
+		this.gl.enableVertexAttribArray(shaderProgram.attributeLocations.get("aTextureCorrds"));
+
+		this.indexBuffer = this.gl.createBuffer();
+		this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, colorBuffer);
+		this.gl.bufferData(this.gl.ELEMENT_ARRAY_BUFFER, new Float32Array(this.indexs), this.gl.STATIC_DRAW);
 	}
 }
